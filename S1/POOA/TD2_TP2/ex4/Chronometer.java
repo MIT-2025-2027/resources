d8829da470cead8ed7b5db68d1a670f4fabf16f8
kfifel <fifel.khalid99@gmail.com>
2025-12-28 17:12:27 +0100

    add the correction of the prof, on the class Chronometer

--- S1/POOA/TD2_TP2/ex4/Chronometer.java
+++ S1/POOA/TD2_TP2/ex4/Chronometer.java
@@ -3,84 +3,77 @@ package TD2_TP2.ex4;
 import java.util.Date;
 
 public class Chronometer implements ChronometreInterface {
-	private final int INIT = 0;
-	private final int RUNNING = 1;
-	private final int STOPPED = 2;
-	
-	private int status;
-	private ChronometerStatus statusEum;
-	private long lastStartDate;
-	private long lastSumDelta;
-	
-	public Chronometer() {
-		this.raz();
-	}
-	
-	@Override
-	public void start() {
-		// if (status != RUNNING) {
-		if (!statusEum.equals(ChronometerStatus.RUNNING)) {
-			Date date = new Date();
-			this.lastStartDate = date.getTime();
-			// this.status = RUNNING;
-			this.statusEum = ChronometerStatus.RUNNING;
-		}
-	}
+    private final static int INIT = 0;
+    private final static int RUNNING = 1;
+    private final static int STOPPED = 2;
 
-	@Override
-	public void stop() {
-		// if (this.status == RUNNING) {
-		if (this.statusEum.equals(ChronometerStatus.RUNNING)) {
-			this.lastSumDelta = this.lastSumDelta + new Date().getTime() - this.lastStartDate;
-			// this.status = STOPPED;
-			this.statusEum = ChronometerStatus.STOPPED;
-		}
-	}
-	 
-	public void throwExecption() {
-		throw new RuntimeException("this is an ex...");
-	}
+    private int state;
+    private Date lastStartDate;
+    private long lastSumDelta;
 
-	@Override
-	public long getTime() {
-		// if (this.status == STOPPED) {
-		if (this.statusEum.equals(ChronometerStatus.STOPPED)) {
-			return this.lastSumDelta;
-		// } else if (this.status == RUNNING) {
-		} else if (this.statusEum.equals(ChronometerStatus.RUNNING)) {
-			return new Date().getTime() - this.lastStartDate + this.lastSumDelta;
-			// start stop => 5s  lastSumDelta 5000;
-			// start -> time lastStartDate time now => 3s
-			// getTime() => now -> lastStartDate + lastSumDelta
- 		} else {
- 			// on a pas encore commancer 
- 			return 0;
- 		}
-		
-		/*
-		 * solution 2
-		 */
-		
-		/*
-			if (this.status == RUNNING) {
-				return new Date().getTime() - this.lastStartDate + this.lastSumDelta;
-				// start stop => 5s  lastSumDelta 5000;
-				// start -> time lastStartDate time now => 3s
-				// getTime() => now -> lastStartDate + lastSumDelta
-	 		} else {
-				return this.lastSumDelta;
-			} 
- 		}
-		 */
-	}
+    public Chronometer() {
+        this.state = INIT;
+        this.lastSumDelta = 0L;
+    }
 
-	@Override
-	public void raz() {
-		// this.status = INIT;
-		this.lastStartDate = 0;
-		this.lastSumDelta = 0;
-		this.statusEum = ChronometerStatus.INIT;
-	}
+    @Override
+    public void start() {
+        switch (this.state) {
+            case INIT:
+                this.lastStartDate = new Date();
+                this.state = RUNNING;
+                break;
+            case RUNNING:
+                break;
+            case STOPPED:
+                this.lastStartDate = new Date();
+                this.state = RUNNING;
+                break;
+        }
 
-	
+    }
+
+    @Override
+    public void stop() {
+        switch (this.state) {
+            case INIT:
+                break;
+            case RUNNING:
+                this.lastSumDelta += new Date().getTime() - this.lastStartDate.getTime();
+                this.state = STOPPED;
+                break;
+            case STOPPED:
+                break;
+        }
+    }
+
+    @Override
+    public long getTime() {
+        switch (this.state) {
+            case INIT:
+                return 0L;
+            case RUNNING:
+                return (this.lastSumDelta + (new Date().getTime() - this.lastStartDate.getTime()));
+            case STOPPED:
+                return this.lastSumDelta;
+            default:
+                return 0L;
+        }
+    }
+
+    @Override
+    public void raz() {
+        switch (this.state) {
+            case INIT:
+                break;
+            case RUNNING:
+                this.state = INIT;
+                this.lastSumDelta = 0;
+                break;
+            case STOPPED:
+                this.state = INIT;
+                this.lastSumDelta = 0;
+                break;
+        }
+    }
 }
